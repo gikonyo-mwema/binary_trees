1fba75a4200c52ef520e8bf38e2a16a53367b691
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-02-28 15:19:46 -0800

    typos fixed

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,7 +8,7 @@
 */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-if (treee == NULL)
+if (tree == NULL)
 return (0);
 
 return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
