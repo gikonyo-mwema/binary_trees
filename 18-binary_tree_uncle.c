d69559f046ae2f46367f08cc47bdd2a53ce831d4
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-02-29 14:38:42 -0800

    betty check

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -4,7 +4,7 @@
 * binary_tree_uncle - finds the uncle of a node
 * @node: pointer to the node to find the uncle
 *
-* Return: pointer to the uncle node. If node is NULL or node has no uncle, return NULL.
+* Return: pointer to the uncle node. Else return NULL.
 */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
